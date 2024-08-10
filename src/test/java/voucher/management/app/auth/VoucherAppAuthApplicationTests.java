3142adaf9f77731acc6cc2444e2471e090bdc411
KhinYadanarNg <e0015793@u.nus.edu>
2024-08-10 19:21:49 +0800

    initial project setup

---
+++ src/test/java/voucher/management/app/auth/VoucherAppAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package voucher.management.app.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoucherAppAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
