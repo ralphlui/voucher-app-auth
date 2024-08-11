8be791d5d5d576f4e558c92673f8fcb60de57a67
KhinYadanarNg <e0015793@u.nus.edu>
2024-08-11 19:14:44 +0800

    added user repository class

---
+++ src/main/java/voucher/management/app/auth/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package voucher.management.app.auth.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import voucher.management.app.auth.entity.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+
+	Page<User> findByIsActiveTrue(Pageable pageable);
+}
