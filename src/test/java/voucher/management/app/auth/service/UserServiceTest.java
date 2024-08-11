939b96c9f8f66d4e2f88c71468cb254adc9e36f7
KhinYadanarNg <e0015793@u.nus.edu>
2024-08-12 00:29:33 +0800

    added unit test case for getting all active users

---
+++ src/test/java/voucher/management/app/auth/service/UserServiceTest.java
@@ -0,0 +1,75 @@
+package voucher.management.app.auth.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+
+import jakarta.transaction.Transactional;
+import voucher.management.app.auth.dto.UserDTO;
+import voucher.management.app.auth.entity.User;
+import voucher.management.app.auth.enums.RoleType;
+import voucher.management.app.auth.repository.UserRepository;
+import voucher.management.app.auth.service.impl.UserService;
+
+@SpringBootTest
+@Transactional
+@ActiveProfiles("test")
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+public class UserServiceTest {
+	
+	private static List<User> mockUsers = new ArrayList<>();
+	
+	@Autowired
+	private UserService userService;
+	
+	@MockBean
+	private UserRepository userRepository;
+	
+	private static User user = new User("admin12345@gmail.com", "Admin", "Pwd@123", RoleType.ADMIN, true);
+
+	@BeforeEach
+	void setUp() {
+		// passwordEncoder = new BCryptPasswordEncoder();
+		// userService = new UserService(userRepository, passwordEncoder);
+
+		mockUsers.add(user);
+
+	}
+
+	@Test
+	void getAllActiveStore() {
+
+		long totalRecord = 0;
+		List<UserDTO> userDTOList = new ArrayList<UserDTO>();
+		Pageable pageable = PageRequest.of(0, 10);
+		Page<User> mockUserPages = new PageImpl<>(mockUsers, pageable, mockUsers.size());
+
+		Mockito.when(userRepository.findByIsActiveTrue(pageable)).thenReturn(mockUserPages);
+		Map<Long, List<UserDTO>> userPages = userService.findByIsActiveTrue(pageable);
+
+		for (Map.Entry<Long, List<UserDTO>> entry : userPages.entrySet()) {
+			totalRecord = entry.getKey();
+			userDTOList = entry.getValue();
+
+		}
+		assertEquals(mockUsers.size(), userDTOList.size());
+		assertEquals(mockUsers.get(0).getEmail(), userDTOList.get(0).getEmail());
+
+	}
+
+}
