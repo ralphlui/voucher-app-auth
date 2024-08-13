ffcfa5cd80637707d1b696ec4bc0776630801785
KhinYadanarNg <e0015793@u.nus.edu>
2024-08-13 19:43:04 +0800

    added password encoder and changed based api name

--- src/main/java/voucher/management/app/auth/configuration/VoucherManagementAuthenticationSecurityConfig.java
+++ src/main/java/voucher/management/app/auth/configuration/VoucherManagementAuthenticationSecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.header.writers.HstsHeaderWriter;
 import org.springframework.security.web.header.writers.StaticHeadersWriter;
@@ -16,7 +18,12 @@ import org.springframework.web.cors.CorsConfiguration;
 @Configuration
 @EnableWebSecurity
 public class VoucherManagementAuthenticationSecurityConfig {
-    private static final String[] SECURED_URLs = { "/api/**" };
+    private static final String[] SECURED_URLs = { "/voucherapp/**" };
+    
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 	
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
