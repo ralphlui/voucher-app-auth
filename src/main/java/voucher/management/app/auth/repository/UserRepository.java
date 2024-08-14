0ac7103c42be7957b5d08571c0c2aa6b760d2486
KhinYadanarNg <e0015793@u.nus.edu>
2024-08-14 22:50:18 +0800

    added findByEmailAndStatus in user repository

--- src/main/java/voucher/management/app/auth/repository/UserRepository.java
+++ src/main/java/voucher/management/app/auth/repository/UserRepository.java
@@ -3,6 +3,7 @@ package voucher.management.app.auth.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import voucher.management.app.auth.entity.User;
@@ -15,4 +16,8 @@ public interface UserRepository extends JpaRepository<User, String> {
 	User save(User user);
 	
 	User findByEmail(String email);
+	
+	@Query("SELECT u FROM User u WHERE u.email = ?1 AND u.isActive = ?2")
+	public User findByEmailAndStatus(String email, boolean isActive,boolean isVerified);
+	
 }
