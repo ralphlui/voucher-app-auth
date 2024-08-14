ad976d665dc70e920e98e4e07a69be9a237451db
KhinYadanarNg <e0015793@u.nus.edu>
2024-08-14 09:40:22 +0800

    added authentication manager in config class

--- src/main/java/voucher/management/app/auth/configuration/VoucherManagementAuthenticationSecurityConfig.java
+++ src/main/java/voucher/management/app/auth/configuration/VoucherManagementAuthenticationSecurityConfig.java
@@ -2,7 +2,8 @@ package voucher.management.app.auth.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -25,6 +26,7 @@ public class VoucherManagementAuthenticationSecurityConfig {
 		return new BCryptPasswordEncoder();
 	}
 	
+
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		return http.cors(cors -> {
@@ -46,4 +48,10 @@ public class VoucherManagementAuthenticationSecurityConfig {
 				.build();
 
 	}
+
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+		return authConfig.getAuthenticationManager();
+	}
+
 }
