369d6d725c126c1f5c72e406ccd5d0cc1aba2bec
KhinYadanarNg <e0015793@u.nus.edu>
2024-08-27 22:11:35 +0800

    removed unused imports

--- src/main/java/voucher/management/app/auth/service/impl/UserService.java
+++ src/main/java/voucher/management/app/auth/service/impl/UserService.java
@@ -5,12 +5,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
