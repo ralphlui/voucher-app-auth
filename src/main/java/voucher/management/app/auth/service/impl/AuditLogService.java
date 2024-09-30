537750d0c39d2554143062fa9d6a2575653d5703
KhinYadanarNg <e0015793@u.nus.edu>
2024-10-01 00:40:59 +0800

    truncate remark if message size is more than max size

--- src/main/java/voucher/management/app/auth/service/impl/AuditLogService.java
+++ src/main/java/voucher/management/app/auth/service/impl/AuditLogService.java
@@ -1,5 +1,7 @@
 package voucher.management.app.auth.service.impl;
 
+import java.nio.charset.StandardCharsets;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -51,11 +53,12 @@ public class AuditLogService implements IAuditService {
 	}
 	
 	
-	private String createLogEntryRequest(String statusCode, String userId, String username, String activityType, String activityDescription,
-			String requestActionEndpoint, String responseStatus, String requestType, String remarks) {
-		
+	private String createLogEntryRequest(String statusCode, String userId, String username, String activityType,
+			String activityDescription, String requestActionEndpoint, String responseStatus, String requestType,
+			String remarks) {
+
 		ObjectMapper objectMapper = new ObjectMapper();
-		AuditLogRequest logRequest = new AuditLogRequest(); 
+		AuditLogRequest logRequest = new AuditLogRequest();
 		logRequest.setStatusCode(statusCode);
 		logRequest.setUserId(userId);
 		logRequest.setUsername(username);
@@ -65,19 +68,64 @@ public class AuditLogService implements IAuditService {
 		logRequest.setResponseStatus(responseStatus);
 		logRequest.setRequestType(requestType);
 		logRequest.setRemarks(remarks);
-		 try {
-	            // Serialize Java object to JSON string
-	            String auditLogString = objectMapper.writeValueAsString(logRequest);
-	            logger.info("Serialized JSON: " + auditLogString);
-	            return auditLogString;
+		try {
 
+			String auditLogString = objectMapper.writeValueAsString(logRequest);
+			logger.info("Serialized JSON: " + auditLogString);
 
-	        } catch (Exception e) {
-	        	logger.error("Exception: Unexpected error occurred while creatin audit log object", e.toString());
-	            e.printStackTrace();
+			byte[] messageBytes = auditLogString.getBytes(StandardCharsets.UTF_8);
+			int messageSize = messageBytes.length;
+			int maxMessageSize = 256 * 1024;  // Max Size 256 KB in bytes
+
+			if (messageSize > maxMessageSize) {
+				logger.warn("Message size exceeds the 256 KB limit: {} bytes, truncating remarks.", messageSize);
+
+				String truncatedRemarks = truncateMessage(logRequest.getRemarks(), maxMessageSize, auditLogString);
+				logRequest.setRemarks(truncatedRemarks.concat("..."));
+
+				auditLogString = objectMapper.writeValueAsString(logRequest);
+				messageBytes = auditLogString.getBytes(StandardCharsets.UTF_8);
+
+				logger.info("Truncated message size: {} bytes", messageBytes.length);
+			}
+
+			return auditLogString;
+
+		} catch (Exception e) {
+			logger.error("Exception: Unexpected error occurred while creatin audit log object", e.toString());
+			e.printStackTrace();
+		}
+		return "";
+
+	}
+	
+	public String truncateMessage(String remarks, int maxMessageSize, String currentMessage) {
+	    try {
+	        // Start truncating the remarks field only if it exceeds the limit
+	        byte[] currentMessageBytes = currentMessage.getBytes(StandardCharsets.UTF_8);
+	        int currentSize = currentMessageBytes.length;
+	        
+	        byte[] remarkBytes = remarks.getBytes(StandardCharsets.UTF_8);
+	        
+	        int remarkSize =remarkBytes.length;
+
+	        int diffMsgSize = currentSize - maxMessageSize;
+
+	        if (diffMsgSize >= remarkSize) {
+	            return ""; // If no space left for remarks, return an empty string
+	        }	      
+	        
+	        int  allowedBytesForRemarks = remarkSize - (diffMsgSize+5);
+	        if (remarkBytes.length <= allowedBytesForRemarks) {
+	            return remarks; 
 	        }
-		 return "";
-		
+
+	        String truncatedRemarks = new String(remarkBytes, 0, allowedBytesForRemarks, StandardCharsets.UTF_8);
+	        return truncatedRemarks;
+	    } catch (Exception e) {
+	        logger.error("Error while truncating message remarks: {}", e.getMessage());
+	        return remarks; 
+	    }
 	}
 	
 }
